07d70e8f71d5e9b530c942547c43f1441255d68b
Freddy Rodriguez <freddy@kangomedia.com>
2013-11-07 17:44:31 -0600

    Add aws-sdk gem

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,17 @@
 class Attachment < ActiveRecord::Base
   belongs_to :attachable, polymorphic: true
-  has_attached_file :file, :styles => { :small => "50x50>", :thumb => "100x100>", :medium => "300x300>" }, :default_url => "/images/default_avatar.png"
+  has_attached_file :file, :styles => { 
+    :small => "50x50>", 
+    :thumb => "100x100>", 
+    :medium => "300x300>" 
+  }, 
+    :default_url => "/images/default_avatar.png", 
+    :storage => :s3, 
+    :s3_credentials => { 
+      :access_key_id => "AKIAJYBWNPT3XLVRPGJA", 
+      :secret_access_key => "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" 
+    }, 
+    :bucket => "2gsapp"
 
 private
   def attachment_params
