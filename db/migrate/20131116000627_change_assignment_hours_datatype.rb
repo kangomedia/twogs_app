7fc82939eb92b605fc596dc7767a8571c11e15c4
Freddy Rodriguez <freddy@kangomedia.com>
2013-11-15 19:02:21 -0600

    Update assignments table

--- db/migrate/20131116000627_change_assignment_hours_datatype.rb
+++ db/migrate/20131116000627_change_assignment_hours_datatype.rb
@@ -1,6 +1,6 @@
 class ChangeAssignmentHoursDatatype < ActiveRecord::Migration
   def up
-    connection.execute($q{
+    connection.execute(%q{
       alter table assignments
       alter column hours
       type integer using cast(hours as integer)
